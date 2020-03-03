c09afaceddbcb4eb5e7c2366a230d8291443708b
Yannik Weiß <yannik.weiss@mailbox.org>
2020-03-03 12:40:03 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if not os.path.exists("timelib.c"):
 
 setup(
     name="strtotime",
-    version="1.0.0",
+    version="1.0.1",
     description="wrapper for timelib",
     author="Ideler IT-Service GmbH",
     author_email="hosting@ideler.de",
